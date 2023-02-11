08411429ce803a494bd5bbb01f26f4ac778c8f38
Dinar Jahan <110754416+DinarPerseus@users.noreply.github.com>
2023-02-12 00:33:50 +0600

    Add files via upload

---
+++ Falsi.cpp
@@ -0,0 +1,29 @@
+//Dinar Perseüs
+//Falsi Method
+#include<bits/stdc++.h>
+using namespace std;
+double f(double x){
+    return (double)(3*x*x-4*x);
+}
+int main(int argc, char const *argv[])
+{
+    double a=1,b=2,c,fa,fb,fc;
+    fa=f(a);
+    fb=f(b);
+    do
+    {
+        c=(a*fb-b*fa)/(fb-fa);
+        fc=f(c);
+        if (fa*fc<0)
+        {
+            fb=fc;
+            b=c;
+        }else
+        {
+            fa=fc;
+            a=c;
+        }
+    } while (fabs(fc)>0.0001);
+    printf("%.6f\n",fc);
+    return 0;
+}
