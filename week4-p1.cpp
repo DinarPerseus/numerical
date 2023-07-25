17b5bffde899a24a1f235b6ad55c624df8050410
Dinar Jahan <110754416+DinarPerseus@users.noreply.github.com>
2023-07-25 20:07:22 +0600

    Create week4-p1.cpp

---
+++ week4-p1.cpp
@@ -0,0 +1,47 @@
+//Dinar Perseüs
+#include<bits/stdc++.h>
+using namespace std;
+int a[1000000], b[1000000];
+void merge(int low,int mid,int high){
+    int h=low,i=low,j=mid+1;
+    while (h<=mid&&j<=high)
+    {
+        if (a[h]>=a[j])
+        {
+            b[i]=a[h];
+            h++;
+        }else
+        {
+            b[i]=a[j];
+            j++;
+        }
+        i++;   
+    }
+    if (h>mid)
+    {
+        for (int k = j; k <= high; k++,i++)b[i]=a[k]; 
+    }else
+    {
+        for (int k = h; k <= mid; k++,i++)b[i]=a[k];
+    } 
+    for (int k = low; k <= high; k++)a[k]=b[k];
+}
+void mergesort(int low,int high){
+    if (low<high)
+    {
+        int mid=(low+high)/2;
+        mergesort(low,mid);
+        mergesort(mid+1,high);
+        merge(low,mid,high);
+    } 
+}
+int main() {
+    int n;cout<<"Enter the size of array:";cin>>n;
+    srand((unsigned) time(NULL));
+    for(int i=0; i<n; i++)a[i]=rand()%(int)1e9;
+    mergesort(0, n-1);
+    int k;cout<<"Enter the value of K:";cin>>k;
+    cout<<k<<" NO largest elements is: "<<a[k-1]<<endl;
+
+    return 0;
+}
